0d5ae852380c344d1743f6a90dedfa5694888e31
soenneker <jake@soenneker.com>
2026-08-29 14:16:46 -0500

    Improve public API XML documentation

--- src/Soenneker.Attio.HttpClients/Registrars/AttioOpenApiHttpClientRegistrar.cs
+++ src/Soenneker.Attio.HttpClients/Registrars/AttioOpenApiHttpClientRegistrar.cs
@@ -13,6 +13,8 @@ public static class AttioOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="AttioOpenApiHttpClient"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddAttioOpenApiHttpClientAsSingleton(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
@@ -24,6 +26,8 @@ public static class AttioOpenApiHttpClientRegistrar
     /// <summary>
     /// Adds <see cref="AttioOpenApiHttpClient"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddAttioOpenApiHttpClientAsScoped(this IServiceCollection services)
     {
         services.AddHttpClientCacheAsSingleton()
